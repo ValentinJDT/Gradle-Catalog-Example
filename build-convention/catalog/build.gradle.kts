867344ec4f79dbd1dbd0e0cdc2b1388b1e623b2f
Valentin JEANDOT <valentin.jeandot@cirly.com>
2024-11-19 15:46:43 +0100

    dependencies versions can be defined in gradle.properties or in build.gradle.kts

--- build-convention/catalog/build.gradle.kts
+++ build-convention/catalog/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.accessors.runtime.addDependencyTo
+
 plugins {
     base
     `version-catalog`
@@ -10,7 +12,7 @@ group = property("group") as String
 catalog {
     versionCatalog {
         // Json dependency :
-        addDependency("serialization-json", "org.json", "json")
+        addDependency("serialization-json", "org.json:json", "20210307")
     }
 }
 
@@ -23,17 +25,27 @@ publishing {
 /**
  * Add a dependency into the catalog.
  *
- * Version definition in gradle.properties : key-version = <version>
- *
  * @param key to use when you need to import dependency in a project
- * @param group of the dependency
- * @param artifactId of the dependency
+ * @param artifact define by group:artifactId
+ * @param version of the dependency, not required if defined in gradle.properties
  */
-private fun VersionCatalogBuilder.addDependency(key: String, group: String, artifactId: String) {
+private fun VersionCatalogBuilder.addDependency(key: String, artifact: String, version: String? = null) {
+    val (group, artifactId) = artifact.split(":")
     val versionKey = "$key-version"
-    if(!hasProperty(versionKey))
-        throw MissingResourceException("Version missing of \"${group}:${artifactId}\". Please add \"$versionKey = <version>\" in gradle.properties.")
 
-    val versionRef = version(key, property(versionKey) as String)
+    val versionRef = if(version != null) {
+
+        if(version.isEmpty())
+            throw IllegalArgumentException("Version of \"$key\" can't be empty. Define it has null if you want to use the version defined in gradle.properties.")
+
+        version(key, version)
+    } else {
+        if(!hasProperty(versionKey))
+            throw MissingResourceException("Version missing of \"${group}:${artifactId}\". Please add \"$versionKey = <version>\" in gradle.properties.")
+
+        version(key, property(versionKey) as String)
+    }
+
     library(key, group, artifactId).versionRef(versionRef)
+
 }
